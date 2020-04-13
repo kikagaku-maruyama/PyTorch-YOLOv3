525edbd67aeeeae98d498ff17f612017692de8b3
dmaruyama-51 <0501awasuram@gmail.com>
2020-04-13 20:52:51 +0900

    fixed

--- test.py
+++ test.py
@@ -21,6 +21,9 @@ import torch.optim as optim
 
 
 def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
+
+    val_losses = []
+
     model.eval()
 
     # Get dataloader
@@ -28,32 +31,43 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
     )
-
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
     labels = []
     sample_metrics = []  # List of tuples (TP, confs, pred)
+
     for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
 
-        # Extract labels
+        # Extract labels(コアラのインデックス)
         labels += targets[:, 1].tolist()
-        # Rescale target
-        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
-        targets[:, 2:] *= img_size
 
         imgs = Variable(imgs.type(Tensor), requires_grad=False)
 
         with torch.no_grad():
-            outputs = model(imgs)
+            loss, outputs = model(imgs, targets)
+            print("loss")
             outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
+
+            val_losses.append(loss)
+
+        # 座標を変換（2: は座標）
+        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+        # オリジナルスケールに戻す
+        targets[:, 2:] *= img_size
 
         sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
 
+    avg_val_losses = torch.tensor(val_losses).mean()
+
     # Concatenate sample statistics
+    if len(sample_metrics) == 0:
+
+        return np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0], dtype=np.int), np.array([0])
+
     true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
     precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
 
-    return precision, recall, AP, f1, ap_class
+    return precision, recall, AP, f1, ap_class, avg_val_losses
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -22,6 +22,8 @@ from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
 
+import warnings
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
@@ -38,32 +40,43 @@ if __name__ == "__main__":
     parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
     opt = parser.parse_args()
     print(opt)
+    
+    warnings.filterwarnings('ignore')
 
-    logger = Logger("logs")
+    # tensorboard logger を使用
+    #logger = Logger("logs")
 
+    # 結果を格納するリストを用意
+    mAPs = []
+    train_losses_tmp = []
+    train_losses = []
+    val_losses = []
+
+    # device の指定
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # output, checkpoints フォルダの作成
     os.makedirs("output", exist_ok=True)
     os.makedirs("checkpoints", exist_ok=True)
 
-    # Get data configuration
+    # data config の取得
     data_config = parse_data_config(opt.data_config)
     train_path = data_config["train"]
     valid_path = data_config["valid"]
     class_names = load_classes(data_config["names"])
 
-    # Initiate model
+    # モデルを読み込み、重みを初期化
     model = Darknet(opt.model_def).to(device)
     model.apply(weights_init_normal)
 
-    # If specified we start from checkpoint
+    # チェックポイント、事前学習済み重みの読み込み
     if opt.pretrained_weights:
         if opt.pretrained_weights.endswith(".pth"):
             model.load_state_dict(torch.load(opt.pretrained_weights))
         else:
             model.load_darknet_weights(opt.pretrained_weights)
 
-    # Get dataloader
+    # dataloader 作成
     dataset = ListDataset(train_path, augment=True, multiscale=opt.multiscale_training)
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -74,8 +87,10 @@ if __name__ == "__main__":
         collate_fn=dataset.collate_fn,
     )
 
+    # 最適化手法の選択
     optimizer = torch.optim.Adam(model.parameters())
 
+    # ログ表示する指標
     metrics = [
         "grid_size",
         "loss",
@@ -93,18 +108,31 @@ if __name__ == "__main__":
         "conf_noobj",
     ]
 
+    # 学習ループ
     for epoch in range(opt.epochs):
+
+        # 訓練モード
         model.train()
+
         start_time = time.time()
+
+        # mini-batch ループ
         for batch_i, (_, imgs, targets) in enumerate(dataloader):
+            
             batches_done = len(dataloader) * epoch + batch_i
 
+            # cuda.FloatTensor に変換
             imgs = Variable(imgs.to(device))
             targets = Variable(targets.to(device), requires_grad=False)
 
+            # 推論 & ロス算出 
+            # output; bbox, conf, cls
             loss, outputs = model(imgs, targets)
             loss.backward()
 
+            train_losses_tmp.append(loss)
+
+            # 累積勾配
             if batches_done % opt.gradient_accumulations:
                 # Accumulates gradient before each step
                 optimizer.step()
@@ -114,8 +142,10 @@ if __name__ == "__main__":
             #   Log progress
             # ----------------
 
+            # ログ表示
             log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, opt.epochs, batch_i, len(dataloader))
 
+            # yolo layer ごとに各種指標を表示（ミニバッチ単位）
             metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
 
             # Log metrics at each YOLO layer
@@ -127,13 +157,13 @@ if __name__ == "__main__":
                 metric_table += [[metric, *row_metrics]]
 
                 # Tensorboard logging
-                tensorboard_log = []
-                for j, yolo in enumerate(model.yolo_layers):
-                    for name, metric in yolo.metrics.items():
-                        if name != "grid_size":
-                            tensorboard_log += [(f"{name}_{j+1}", metric)]
-                tensorboard_log += [("loss", loss.item())]
-                logger.list_of_scalars_summary(tensorboard_log, batches_done)
+                #tensorboard_log = []
+                #for j, yolo in enumerate(model.yolo_layers):
+                #    for name, metric in yolo.metrics.items():
+                #        if name != "grid_size":
+                #            tensorboard_log += [(f"{name}_{j+1}", metric)]
+                #tensorboard_log += [("loss", loss.item())]
+                #logger.list_of_scalars_summary(tensorboard_log, batches_done)
 
             log_str += AsciiTable(metric_table).table
             log_str += f"\nTotal loss {loss.item()}"
@@ -146,11 +176,15 @@ if __name__ == "__main__":
             print(log_str)
 
             model.seen += imgs.size(0)
-
+        
+        avg_train_loss = torch.tensor(train_losses_tmp).mean()
+        train_losses.append(avg_train_loss)
+        
+        # 検証データへの准電波
         if epoch % opt.evaluation_interval == 0:
             print("\n---- Evaluating Model ----")
             # Evaluate the model on the validation set
-            precision, recall, AP, f1, ap_class = evaluate(
+            precision, recall, AP, f1, ap_class, avg_val_loss = evaluate(
                 model,
                 path=valid_path,
                 iou_thres=0.5,
@@ -159,13 +193,20 @@ if __name__ == "__main__":
                 img_size=opt.img_size,
                 batch_size=8,
             )
+
+            # val_loss をエポック単位で保存
+            val_losses.append(avg_val_loss)
+
+
             evaluation_metrics = [
                 ("val_precision", precision.mean()),
                 ("val_recall", recall.mean()),
                 ("val_mAP", AP.mean()),
                 ("val_f1", f1.mean()),
             ]
-            logger.list_of_scalars_summary(evaluation_metrics, epoch)
+
+            #tensorboard logger に追加
+            #logger.list_of_scalars_summary(evaluation_metrics, epoch)
 
             # Print class APs and mAP
             ap_table = [["Index", "Class name", "AP"]]
@@ -174,5 +215,28 @@ if __name__ == "__main__":
             print(AsciiTable(ap_table).table)
             print(f"---- mAP {AP.mean()}")
 
+            # mAP をリストに追加
+            mAPs.append(AP.mean())
+
+
         if epoch % opt.checkpoint_interval == 0:
             torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
+
+    # 学習結果の表示
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    # mAP
+    mAPs = torch.tensor(mAPs).numpy()
+    result = pd.DataFrame({"mAP" : mAPs})
+    result.plot()
+    plt.savefig("mAP.png")
+
+    # losses
+    train_losses = torch.tensor(train_losses).numpy()
+    val_losses = torch.tensor(val_losses).numpy()
+
+    result = pd.DataFrame({"train_loss": train_losses, "val_loss": val_losses})
+    result.plot()
+    plt.savefig("losses.png")
+
